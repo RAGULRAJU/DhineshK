0930a1b67716f470bd841468f44618e6e6e3abe5
RR <96754952+RAGULRAJU@users.noreply.github.com>
2022-08-31 12:15:07 +0530

    Add files via upload

---
+++ age.js
@@ -0,0 +1,32 @@
+//alert("Welcome to the Age Calculator")
+function calculateAge() {
+    var d1 = document.getElementById('date').value;
+    var m1 = document.getElementById('month').value;
+    var y1 = document.getElementById('year').value;
+     var date = new Date();
+    var d2 = date.getDate();
+    var m2 = 1 + date.getMonth();
+    var y2 = date.getFullYear();
+    var month = [ 31, 28, 31, 30, 31, 30 , 31 , 31, 30 , 31 , 30 , 31 ];
+    if (d1 > d2) {
+        d2 = d2 + month[m2 - 1];
+        m2 = m2 - 1;
+    }
+    if (m1 > m2) {
+        m2 = m2 + 12;
+        y2 = y2 - 1;
+    }
+    var d = d2 - d1;
+    var m = m2 - m1;
+    var y = y2 - y1;
+    document.getElementById("displayAge").innerHTML='Your Age is '+y+' years '+m+' months '+d+' day';
+    
+    if(d1==0 || m1==0|| y1==0 )
+    {
+        document.getElementById("displayAge").innerHTML=' Enter the date of birth properly';
+    }
+    if(y1>y2)
+    {
+        document.getElementById("displayAge").innerHTML='Date of Brith must be earlier than today`s date';
+    }
+}
